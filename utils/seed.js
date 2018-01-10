aa886be3cdc743ccafb9728040b4d11c1797d235
Benson Maruchu <benmaruchu@gmail.com>
2018-01-10 10:30:21 +0300

    add seed function

---
+++ utils/seed.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * This function seed  a collection of priorities provided to it as the first
+ *  argument.
+ * @function
+ * @param {Array|Object} priorities - Priority collection or object to seed
+ * @param {Function} done - Callback when the function finished seeding data
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+
+//  dependencies
+const path = require('path');
+const _ = require('lodash');
+const Priority = require(path.join(__dirname, '..', 'models',
+  'priority'));
+
+module.exports = function (priorities, done) {
+
+  priorities = _.concat([], priorities);
+
+  Priority.create(priorities, function (error, results) {
+
+    if (error) {
+
+      done(error);
+    }
+
+    done(null, results);
+  });
+};
