d3b482beca4221d002f60f1a042df719495b38f0
Benson Maruchu <benmaruchu@gmail.com>
2018-05-26 15:47:54 +0300

    Change author email extension from @mail to @gmail

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
  * in order of their importance.
  *
  * @author Benson Maruchu <benmaruchu@gmail.com>
- * @author lally elias <lallyelias87@mail.com>
+ * @author lally elias <lallyelias87@gmail.com>
  * @since  0.1.0
  * @version 0.1.0
  * @license MIT
