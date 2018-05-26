d319f8fcf14952469c32f06a8a69e2a17572ab02
Benson Maruchu <benmaruchu@gmail.com>
2018-05-26 16:05:04 +0300

    Add API documentation
    - Update apidoc.json to use description and version from package.json
    - Generate apidoc

---
+++ docs/api_project.js
@@ -0,0 +1,20 @@
+define({
+  "name": "majifix-priority",
+  "version": "1.0.0",
+  "description": "A representation an entity which provides a way to prioritize service and service request(issues) in order of their importance.",
+  "title": "Majifix - Priority",
+  "url": "https://majifix-priority.herokuapp.com/v1",
+  "sampleUrl": "https://majifix-priority.herokuapp.com/v1",
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-05-26T13:03:18.228Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
