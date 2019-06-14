97c8ab4edbcd664da3f27b881a4a40937495c2ea
rijkerd <richardaggrey7@gmail.com>
2019-06-14 06:34:23 +0300

    refactor(examples): fix and add seed

--- examples/app.js
+++ examples/app.js
@@ -1,80 +1,29 @@
-'use strict';
+// dependencies
+const app = require('@lykmapipo/express-common');
+const { connect, jsonSchema } = require('@lykmapipo/mongoose-common');
+const { router, info, apiVersion } = require('../lib/index');
 
+app.mount(router);
 
-/* ensure mongo uri */
-process.env.MONGODB_URI =
-  (process.env.MONGODB_URI) || 'mongodb://localhost/majifix-priority';
+connect(connectionError => {
+  if (connectionError) {
+    throw connectionError;
+  }
 
-
-/* dependencies */
-const path = require('path');
-const _ = require('lodash');
-const async = require('async');
-const mongoose = require('mongoose');
-// mongoose.set('debug', true);
-const { start } = require('@lykmapipo/express-common');
-const { Jurisdiction } = require('@codetanzania/majifix-jurisdiction');
-const {
-  Priority,
-  info,
-  apiVersion,
-  app
-} = require(path.join(__dirname, '..', 'index'));
-let samples = require('./samples')(20);
-
-/* connect to mongoose */
-mongoose.connect(process.env.MONGODB_URI);
-
-
-function boot() {
-
-  async.waterfall([
-
-    function clear(next) {
-      Priority.remove(function () {
-        next();
-      });
-    },
-
-    function clear(next) {
-      Jurisdiction.remove(function () {
-        next();
-      });
-    },
-
-    function seedJurisdiction(next) {
-      const jurisdiction = Jurisdiction.fake();
-      jurisdiction.post(next);
-    },
-
-    function seed(jurisdiction, next) {
-      /* fake priorities */
-      samples =
-        _.map(samples, function (sample) {
-          if ((sample.weight % 2 === 0)) {
-            sample.jurisdiction = jurisdiction;
-          }
-          return sample;
-        });
-      Priority.create(samples, next);
-    }
-
-  ], function (error, results) {
-
-    /* expose module info */
-    app.get('/', function (request, response) {
-      response.status(200);
-      response.json(info);
-    });
-
-    /* fire the app */
-    start(function (error, env) {
-      console.log(
-        `visit http://0.0.0.0:${env.PORT}/${apiVersion}/priorities`
-      );
-    });
-
+  app.get('/', (request, response) => {
+    response.status(200);
+    response.json(info);
   });
-}
 
-boot();
+  app.get(`/${apiVersion}/schemas`, (request, response) => {
+    const schema = jsonSchema();
+    response.status(200);
+    response.json(schema);
+  });
+
+  /* fire the app */
+  app.start((error, env) => {
+    // eslint-disable-next-line no-console
+    console.log(`visit http://0.0.0.0:${env.PORT}/${apiVersion}/priorities`);
+  });
+});
