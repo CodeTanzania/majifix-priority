84ad7f634effd416ec23c13916de3a735078c95b
rijkerd <richardaggrey7@gmail.com>
2019-08-27 10:35:28 +0300

    test(integration): normalize priority delete

--- test/integration/priority.delete.spec.js
+++ test/integration/priority.delete.spec.js
@@ -2,71 +2,72 @@ import { Jurisdiction } from '@codetanzania/majifix-jurisdiction';
 import { clear, create, expect } from '@lykmapipo/mongoose-test-helpers';
 import { Priority } from '../../src/index';
 
-describe('Priority', () => {
-  const jurisdiction = Jurisdiction.fake();
+describe('Priority Static Delete', () => {
+  before(done => clear(done));
 
-  before(done => clear(Jurisdiction, Priority, done));
+  const jurisdiction = Jurisdiction.fake();
 
   before(done => create(jurisdiction, done));
 
-  describe('static delete', () => {
-    let priority;
+  let priority;
 
-    before(done => {
-      priority = Priority.fake();
-      priority.jurisdiction = jurisdiction;
-      create(priority, done);
-    });
+  before(done => {
+    priority = Priority.fake();
+    priority.jurisdiction = jurisdiction;
+    create(priority, done);
+  });
 
-    it('should be able to delete', done => {
-      Priority.del(priority._id, (error, deleted) => {
-        expect(error).to.not.exist;
-        expect(deleted).to.exist;
-        expect(deleted._id).to.eql(priority._id);
-        done(error, deleted);
-      });
-    });
-
-    it('should throw error if not exists', done => {
-      Priority.del(priority._id, (error, deleted) => {
-        expect(error).to.exist;
-        // expect(error.status).to.exist;
-        expect(error.name).to.be.equal('DocumentNotFoundError');
-        expect(deleted).to.not.exist;
-        done();
-      });
+  it('should be able to delete', done => {
+    Priority.del(priority._id, (error, deleted) => {
+      expect(error).to.not.exist;
+      expect(deleted).to.exist;
+      expect(deleted._id).to.eql(priority._id);
+      done(error, deleted);
     });
   });
 
-  describe('instance delete', () => {
-    let priority;
-
-    before(done => {
-      priority = Priority.fake();
-      priority.post((error, created) => {
-        priority = created;
-        done(error, created);
-      });
-    });
-
-    it('should be able to delete', done => {
-      priority.del((error, deleted) => {
-        expect(error).to.not.exist;
-        expect(deleted).to.exist;
-        expect(deleted._id).to.eql(priority._id);
-        done(error, deleted);
-      });
-    });
-
-    it('should not throw if not exists', done => {
-      priority.del((error, deleted) => {
-        expect(error).to.not.exist;
-        expect(deleted).to.exist;
-        expect(deleted._id).to.eql(priority._id);
-        done();
-      });
+  it('should throw error if not exists', done => {
+    Priority.del(priority._id, (error, deleted) => {
+      expect(error).to.exist;
+      // expect(error.status).to.exist;
+      expect(error.name).to.be.equal('DocumentNotFoundError');
+      expect(deleted).to.not.exist;
+      done();
     });
   });
 
-  after(done => clear(Jurisdiction, Priority, done));
+  after(done => clear(done));
+});
+
+describe('Priority Instance delete', () => {
+  before(done => clear(done));
+  let priority;
+
+  before(done => {
+    priority = Priority.fake();
+    priority.post((error, created) => {
+      priority = created;
+      done(error, created);
+    });
+  });
+
+  it('should be able to delete', done => {
+    priority.del((error, deleted) => {
+      expect(error).to.not.exist;
+      expect(deleted).to.exist;
+      expect(deleted._id).to.eql(priority._id);
+      done(error, deleted);
+    });
+  });
+
+  it('should not throw if not exists', done => {
+    priority.del((error, deleted) => {
+      expect(error).to.not.exist;
+      expect(deleted).to.exist;
+      expect(deleted._id).to.eql(priority._id);
+      done();
+    });
+  });
+
+  after(done => clear(done));
 });
